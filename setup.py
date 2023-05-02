aa1ee9e98bc1d559646fe296938e8ceaada06457
Andrew Powers-Holmes <aholmes@omnom.net>
2023-05-02 05:21:53 +0000

    move to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name='taming',
-    version='0.0.2',
-    description='Taming Transformers for High-Resolution Image Synthesis',
-    packages=find_packages(),
-    install_requires=[
-        'torch',
-        'numpy',
-        'tqdm',
-    ],
-)
+setuptools.setup()
